d94c19ff818ebab6ec4130baf8511ff4abe676b1
hsuhsulin <dt20250325@gmail.com>
2025-06-02 16:12:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from linebot.v3.messaging import (
 )
 from linebot.v3.webhooks import MessageEvent, TextMessageContent, LocationMessageContent, PostbackEvent
 from linebot.models.send_messages import ImageSendMessage
+from flask import send_from_directory
 
 app = Flask(__name__)
 
@@ -26,6 +27,10 @@ configuration = Configuration(access_token=os.getenv("LINE_CHANNEL_ACCESS_TOKEN"
 handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 api_client = ApiClient(configuration)
 api = MessagingApi(api_client)
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
 
 # 全域狀態變數
 Weather_Func = False
